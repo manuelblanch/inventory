bb49a3d5a2962046cc0eb08e73b4b88d339e97f4
manuelblanch <manuelblanch@iesebre.com>
2016-12-01 21:05:05 +0100

    pujada

--- src/Providers/InventoryServiceProvider.php
+++ src/Providers/InventoryServiceProvider.php
@@ -1,18 +1,69 @@
 <?php
-
 namespace Scool\Inventory\Providers;
-
+use Acacha\Names\Providers\NamesServiceProvider;
 use Illuminate\Support\ServiceProvider;
-
+use Scool\Inventory\ScoolInventory;
+/**
+ * Class InventoryServiceProvider.
+ *
+ * @package Scool\Inventory\Providers
+ */
 class InventoryServiceProvider extends ServiceProvider
 {
+    /**
+     * Register package services.
+     */
     public function register()
     {
-        
+        if (!defined('SCOOL_INVENTORY_PATH')) {
+            define('SCOOL_INVENTORY_PATH', realpath(__DIR__.'/../../'));
+        }
+        $this->app->register(NamesServiceProvider::class);
     }
-
+    /**
+     * Bootstrap package services.
+     *
+     * @return void
+     */
     public function boot()
     {
-      $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');  
+        $this->loadMigrations();
+        $this->publishFactories();
+        $this->publishConfig();
+        $this->publishTests();
+    }
+    /**
+     * Load migrations.
+     */
+    private function loadMigrations()
+    {
+        $this->loadMigrationsFrom(SCOOL_CURRICULUM_PATH . '/database/migrations');
+    }
+    /**
+     * Publish factories.
+     */
+    private function publishFactories()
+    {
+        $this->publishes(
+            ScoolCurriculum::factories(),"scool_curriculum"
+        );
+    }
+    /**
+     * Publish config.
+     */
+    private function publishConfig() {
+        $this->publishes(
+            ScoolCurriculum::configs(),"scool_curriculum"
+        );
+        $this->mergeConfigFrom(
+            SCOOL_CURRICULUM_PATH . '/config/curriculum.php', 'scool_curriculum'
+        );
+    }
+    private function publishTests()
+    {
+        $this->publishes(
+            [SCOOL_CURRICULUM_PATH .'/tests/CurriculumTest.php' => 'tests/CurriculumTest.php'] ,
+            'scool_curriculum'
+        );
     }
 }
