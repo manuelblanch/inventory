d71731929ccfabf470c9b6bb7e06f198b43049dc
manuelblanch <manuelblanch@iesebre.com>
2017-02-21 18:00:59 +0100

    seedprovider

--- src/Providers/InventoryServiceProvider.php
+++ src/Providers/InventoryServiceProvider.php
@@ -1,14 +1,16 @@
 <?php
-
 namespace Scool\Inventory\Providers;
-
 use Acacha\Names\Providers\NamesServiceProvider;
+use Acacha\Stateful\Providers\StatefulServiceProvider;
 use Illuminate\Support\ServiceProvider;
-use Acacha\Stateful\Providers\StatatefulServiceProvider;
 use Scool\Inventory\ScoolInventory;
-
+use Scool\Inventory\Stats\CacheableStatsRepository;
+use Scool\Inventory\Stats\Contracts\StatsRepository as StatsRepositoryInterface;
+use Scool\Inventory\Stats\StatsRepository;
 /**
  * Class InventoryServiceProvider.
+ *
+ * @package Scool\Inventory\Providers
  */
 class InventoryServiceProvider extends ServiceProvider
 {
@@ -20,11 +22,40 @@ class InventoryServiceProvider extends ServiceProvider
         if (!defined('SCOOL_INVENTORY_PATH')) {
             define('SCOOL_INVENTORY_PATH', realpath(__DIR__.'/../../'));
         }
-        $this->app->register(NamesServiceProvider::class);
-        $this->app->bind(\Scool\Inventory\Repositories\StudyRepository::class, \Scool\Inventory\Repositories\StudyRepositoryEloquent::class);
+        $this->registerNamesServiceProvider();
+        $this->registerStatefulEloquentServiceProvider();
+        $this->bindRepositories();
         $this->app->bind(StatsRepositoryInterface::class,function() {
             return new CacheableStatsRepository(new StatsRepository());
         });
+    }
+    /**
+     * Bind repositories
+     */
+    protected function bindRepositories()
+    {
+        $this->app->bind(
+            \Scool\Inventory\Repositories\StudyRepository::class,
+            \Scool\Inventory\Repositories\StudyRepositoryEloquent::class);
+        $this->app->bind(\Scool\Inventory\Repositories\TodoRepository::class, \Scool\Inventory\Repositories\TodoRepositoryEloquent::class);
+        $this->app->bind(\Scool\Inventory\Repositories\ShitRepository::class, \Scool\Inventory\Repositories\ShitRepositoryEloquent::class);
+        //:end-bindings:
+    }
+    /**
+     * Register acacha/stateful-eloquent Service Provider.
+     *
+     */
+    protected function registerStatefulEloquentServiceProvider()
+    {
+        $this->app->register(StatefulServiceProvider::class);
+    }
+    /**
+     * Register acacha/names Service Provider.
+     *
+     */
+    protected function registerNamesServiceProvider()
+    {
+        $this->app->register(NamesServiceProvider::class);
     }
     /**
      * Bootstrap package services.
@@ -35,16 +66,36 @@ class InventoryServiceProvider extends ServiceProvider
     {
         $this->defineRoutes();
         $this->loadMigrations();
+        $this->loadViews();
         $this->publishFactories();
         $this->publishConfig();
         $this->publishTests();
+    }
+    /**
+     * Define the inventory routes.
+     */
+    protected function defineRoutes()
+    {
+        if (!$this->app->routesAreCached()) {
+            $router = app('router');
+            $router->group(['namespace' => 'Scool\Inventory\Http\Controllers'], function () {
+                require __DIR__.'/../Http/routes.php';
+            });
+        }
+    }
+    /**
+     * Load package views.
+     */
+    private function loadViews()
+    {
+        $this->loadViewsFrom(SCOOL_INVENTORY_PATH . '/resources/views', 'inventory');
     }
     /**
      * Load migrations.
      */
     private function loadMigrations()
     {
-        $this->loadMigrationsFrom(SCOOL_INVENTORY_PATH.'/database/migrations');
+        $this->loadMigrationsFrom(SCOOL_INVENTORY_PATH . '/database/migrations');
     }
     /**
      * Publish factories.
@@ -52,43 +103,28 @@ class InventoryServiceProvider extends ServiceProvider
     private function publishFactories()
     {
         $this->publishes(
-            ScoolInventory::factories(), 'scool_inventory'
+            ScoolInventory::factories(),"scool_inventory"
         );
     }
     /**
      * Publish config.
      */
-    private function publishConfig()
-    {
+    private function publishConfig() {
         $this->publishes(
-            ScoolInventory::configs(), 'scool_inventory'
+            ScoolInventory::configs(),"scool_inventory"
         );
         $this->mergeConfigFrom(
-            SCOOL_INVENTORY_PATH.'/config/inventory.php', 'scool_inventory'
+            SCOOL_INVENTORY_PATH . '/config/inventory.php', 'scool_inventory'
         );
     }
+    /**
+     * Publich tests.
+     */
     private function publishTests()
     {
         $this->publishes(
-            [SCOOL_INVENTORY_PATH.'/tests/InventoryTest.php' => 'tests/InventoryTest.php'],
-
+            [SCOOL_INVENTORY_PATH .'/tests/InventoryTest.php' => 'tests/InventoryTest.php'] ,
             'scool_inventory'
-
         );
-    }
-
-    protected function defineRoutes()
-    {
-        if (!$this->app->routesAreCached()) {
-            $router = app('router');
-            $router->group(['namespace' => 'Scool\Inventory\Http\Controllers'], function () {
-            require __DIR__.'/../Http/routes.php';
-            });
-        }
-    }
-
-    private function registerStatefulEloquentServiceProvider()
-    {
-        $this->app->register(StatefulServiceProvider::class);
     }
 }
