66319d82b31a395e010635c271638fe6bb79b977
manuelblanch <manuelblanch@iesebre.com>
2017-12-04 20:06:59 +0100

    notification

---
+++ src/Http/Controllers/SocialAuthFacebookController.php
@@ -0,0 +1,34 @@
+<?php
+
+// SocialAuthFacebookController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Socialite;
+use App\Services\SocialFacebookAccountService;
+
+class SocialAuthFacebookController extends Controller
+{
+  /**
+   * Create a redirect method to twitter api.
+   *
+   * @return void
+   */
+    public function redirect()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    /**
+     * Return a callback method from twitter api.
+     *
+     * @return callback URL from twitter
+     */
+    public function callback(SocialFacebookAccountService $service)
+    {
+        $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
+        auth()->login($user);
+        return redirect()->to('/home');
+    }
+}
